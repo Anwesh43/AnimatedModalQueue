35dc9fa88616476a7214286cba7e9706a2e2d22a
Anwesh43 <anweshthecool0@gmail.com>
2017-12-12 23:14:39 +0530

    adding fixes in modal

--- Modal.js
+++ Modal.js
@@ -1,9 +1,9 @@
 const w = window.innerWidth,h = window.innerHeight
 class Modal {
-    constructor(text) {
+    constructor(text,color) {
         this.text = text
         this.maxY = h/2-h/6
-        this.animator = new ModalAnimator(this)
+        this.color = color
     }
     createDom() {
         this.div = document.createElement('div')
@@ -11,14 +11,20 @@ class Modal {
         this.div.style.height = h/3
         this.div.style.position = 'absolute'
         this.div.style.left = w/2 - w/6
-        this.div.style.top = h - h/6
-        this.div.style.background = '#EEEEEE'
-        this.div.style.borderRadius = Math.min(w,h)/12
+        this.div.style.top = h+ h/3
+        this.div.style.background = this.color || '#EEEEEE'
+        this.div.style.borderRadius = `${Math.min(w,h)/25}px`
+        this.div.style.fontSize = Math.min(w,h)/16
+        this.div.style.textAlign = 'center'
         this.div.innerHTML = this.text
+        this.animator = new ModalAnimator(this)
     }
     addToParent() {
         document.body.appendChild(this.div)
         this.animator.startAnimation()
+    }
+    setY(y) {
+        this.div.style.top = y
     }
 }
 class ModalAnimator {
@@ -28,15 +34,21 @@ class ModalAnimator {
         this.y = parseInt(modal.div.style.top)
         this.oy = this.y
         this.destY = modal.maxY
+        this.modal = modal
     }
     startAnimation() {
         if(!this.animated) {
             this.animated = true
             this.state.startUpdating()
             const interval = setInterval(()=>{
-                this.state.update(()=>{
-                    this.y = this.oy + (this.destY-this.y)*scale
+                this.state.update((scale)=>{
+                    this.y = this.oy + (this.destY-this.oy)*scale
+                    this.modal.setY(this.y)
                 })
+                if(this.state.stopped()) {
+                    clearInterval(interval)
+                    this.animated = false
+                }
             },50)
         }
     }
@@ -45,10 +57,20 @@ class ModalState {
     constructor() {
         this.scale = 0
         this.dir = 0
+        this.prevScale = 0
     }
     update(cb) {
         this.scale += this.dir*0.1
+        console.log(this.scale)
         cb(this.scale)
+        if(Math.abs(this.prevScale-this.scale)>1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+        }
+    }
+    stopped() {
+        return this.dir == 0
     }
     startUpdating() {
         this.dir = 1-2*this.scale
